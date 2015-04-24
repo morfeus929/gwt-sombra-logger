e244dd37e3cd83f3b6fb310a571a18f853833137
Edgar <sombradev@gmail.com>
2015-04-24 17:49:26 +0300

    add possibility change location of deploy folder

--- src/main/java/com/sombrainc/gwt/sombra/logger/server/AbstractLogServiceServlet.java
+++ src/main/java/com/sombrainc/gwt/sombra/logger/server/AbstractLogServiceServlet.java
@@ -13,10 +13,15 @@ public abstract class AbstractLogServiceServlet extends RemoteServiceServlet imp
 	@Override
 	public void logClientException(SerializableThrowable ex, String userAgent, String platform, String moduleName) {
 		final HttpServletRequest request = getThreadLocalRequest();
-		final String symbolMapsDirectory = request.getServletContext().getRealPath("/WEB-INF/deploy/" + moduleName + "/symbolMaps");
+		final String symbolMapsDirectory = getPathToDeployFolder(moduleName, request);
 		final String permutationName = request.getHeader(RpcRequestBuilder.STRONG_NAME_HEADER);
 		StackTraceDeobfuscator.fromFileSystem(symbolMapsDirectory).deobfuscateStackTrace(ex, permutationName);
 		logException(ex, request, userAgent, platform);
+	}
+
+	public String getPathToDeployFolder(String moduleName, final HttpServletRequest request) {
+		final String symbolMapsDirectory = request.getServletContext().getRealPath("/WEB-INF/deploy/" + moduleName + "/symbolMaps");
+		return symbolMapsDirectory;
 	}
 
 	protected abstract void logException(SerializableThrowable e, HttpServletRequest request, String userAgent, String platform);
